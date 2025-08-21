3e3e9daa41cba998da19e9d979e5eeff21fc09df
Mariana20209 <mariasuarez20209@gmail.com>
2025-08-20 23:10:08 -0500

    Agregar información personal y ejercicios básicos de JavaScript

---
+++ src/ejercicio2.js
@@ -0,0 +1,9 @@
+const nume1 = 14;
+const num2 = 9;
+if (num1 > num2){
+    console.log("El mayor es:" +num1);
+} else if (num2>num1){
+    console.log("El mayor es:"+num2);
+}else {
+    console.log("Ambos son iguales");
+}
---
+++ src/ejercicio3.js
@@ -0,0 +1,9 @@
+const edad=15;
+if (edad>=0 && edad <=12);{
+console.log("Niño");
+}else if (edad >=13 && edad <=17){
+console.log("Adolescente");
+}else if (edad >=18 && edad <=64){
+console.log("Adolescente");
+}else if (edad >=65 );
+console.log("Anciano");
