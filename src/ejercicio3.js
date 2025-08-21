79530cae8ed9dc9a4f77a42b8db25f7862ecece6
Mariana20209 <mariasuarez20209@gmail.com>
2025-08-20 23:19:22 -0500

    Actualizar lógica de clasificación por edad en ejercicio3.js

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,9 +1,11 @@
-const edad=15;
-if (edad>=0 && edad <=12);{
-console.log("Niño");
-}else if (edad >=13 && edad <=17){
-console.log("Adolescente");
-}else if (edad >=18 && edad <=64){
-console.log("Adolescente");
-}else if (edad >=65 );
-console.log("Anciano");
+const edad = 15;
+
+if (edad >= 0 && edad <= 12) {
+  console.log("niño");
+} else if (edad >= 13 && edad <= 17) {
+  console.log("adolescente");
+} else if (edad >= 18 && edad <= 64) {
+  console.log("adulto");
+} else if (edad >= 65) {
+  console.log("anciano");
+}
